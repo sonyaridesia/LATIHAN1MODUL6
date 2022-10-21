f1ac9f26ccbe490afed906214d617adc3f04b6af
sonyaridesia <sonyaridesia03@gmail.com>
2022-10-21 15:09:43 +0700

    Create ALGO MODUL 6.py

---
+++ ALGO MODUL 6.py
@@ -0,0 +1,54 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Oct 21 14:10:06 2022
+
+@author: sonyaridesia
+"""
+
+def kategori(n = 0, u = 1, l = 0):
+    while (u == 1):
+        r = input("\nMasukkan nilai = ")
+    
+        h = n
+        t = h + n
+        l += 1
+
+        if (r == "A"):
+            n = 4.00
+            print("Nilai = ",n)
+        elif (r == "A-"):
+            n = 3.75
+            print("Nilai = ",n)
+        elif (r == "B+"):
+            n = 3.50
+            print("Nilai = ",n)
+        elif (r == "B"):
+            n = 3.00
+            print("Nilai = ",n)
+        elif (r == "B-"):
+            n = 2.75
+            print("Nilai = ",n)
+        elif (r == "C+"):
+            n = 2.50
+            print("Nilai = ",n)
+        elif (r == "C"):
+            n = 2.00
+            print("Nilai = ",n)
+        elif (r == "C-"):
+            n = 1.75
+            print("Nilai = ",n)
+        elif (r == "D"):
+            n = 1.50
+            print("Nilai = ",n)
+        elif (r == "E"):
+            n = 1.25
+            print("Nilai = ",n)
+        else :
+            u = 0
+            
+    O = t/l
+    print("Rata-ratanya adalah = ", O)
+
+rata = kategori()
+print(rata)
